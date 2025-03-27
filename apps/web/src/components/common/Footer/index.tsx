9da66dbe16bc7433f932479c50025bc8fd8fd1a5
ajimeno04 <jimenobeltran11@gmail.com>
2025-03-27 08:12:06 +0100

    refactor: Clean up Footer and Header components by removing conditional rendering and restoring previously commented code

--- apps/web/src/components/common/Footer/index.tsx
+++ apps/web/src/components/common/Footer/index.tsx
@@ -46,38 +46,32 @@ const Footer = (): ReactElement | null => {
   return (
     <footer className={css.container}>
       <ul>
-        {isOfficialHost ? (
-          <>
-            {/* <li>
-              <Typography variant="caption">&copy;2022–{new Date().getFullYear()} Core Contributors GmbH</Typography>
-            </li>
-            <li>
-              <FooterLink href={getHref(AppRoutes.terms)}>Terms</FooterLink>
-            </li>
-            <li>
-              <FooterLink href={getHref(AppRoutes.privacy)}>Privacy</FooterLink>
-            </li>
-            <li>
-              <FooterLink href={getHref(AppRoutes.licenses)}>Licenses</FooterLink>
-            </li>
-            <li>
-              <FooterLink href={getHref(AppRoutes.imprint)}>Imprint</FooterLink>
-            </li>
-            <li>
-              <FooterLink href={getHref(AppRoutes.cookie)}>Cookie policy</FooterLink>
-            </li>
-            <li>
-              <FooterLink href={getHref(AppRoutes.settings.index)}>Preferences</FooterLink>
-            </li>
-            <li>
-              <ExternalLink href={HELP_CENTER_URL} noIcon sx={{ span: { textDecoration: 'underline' } }}>
-                Help
-              </ExternalLink>
-            </li> */}
-          </>
-        ) : (
-          <li>This is an unofficial distribution of the app</li>
-        )}
+        <li>
+          <Typography variant="caption">&copy;2022–{new Date().getFullYear()} Core Contributors GmbH</Typography>
+        </li>
+        <li>
+          <FooterLink href={getHref(AppRoutes.terms)}>Terms</FooterLink>
+        </li>
+        <li>
+          <FooterLink href={getHref(AppRoutes.privacy)}>Privacy</FooterLink>
+        </li>
+        <li>
+          <FooterLink href={getHref(AppRoutes.licenses)}>Licenses</FooterLink>
+        </li>
+        <li>
+          <FooterLink href={getHref(AppRoutes.imprint)}>Imprint</FooterLink>
+        </li>
+        <li>
+          <FooterLink href={getHref(AppRoutes.cookie)}>Cookie policy</FooterLink>
+        </li>
+        <li>
+          <FooterLink href={getHref(AppRoutes.settings.index)}>Preferences</FooterLink>
+        </li>
+        <li>
+          <ExternalLink href={HELP_CENTER_URL} noIcon sx={{ span: { textDecoration: 'underline' } }}>
+            Help
+          </ExternalLink>
+        </li>
 
         <li>
           <ExternalLink href={`${packageJson.homepage}/releases/tag/v${packageJson.version}`} noIcon>
